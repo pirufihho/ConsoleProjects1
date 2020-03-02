ce122bdd417c87369e7b9328a46315d0c81e3e7a
fvalle <fvalle@ticketmundo.com>
2020-03-02 13:50:20 -0300

    //Cambios en ejercicios

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -11,6 +12,10 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             solution(1041);
+
+            solution("Codility");
+
+            solution2(54321);
 
             // Step 1: Array Declaration 
             string[] stringarr;
@@ -72,6 +77,53 @@ namespace ConsoleApp1
             }
 
             return longest;
+        }
+
+        public static string solution(string S)
+        {
+            // write your code in C# 6.0 with .NET 4.5 (Mono)
+            List<char> repeatedCharacters = new List<char>();
+            var groupsOfChars = S.ToUpper().GroupBy(oChar => oChar);
+            groupsOfChars.ToList().ForEach(item =>
+            {
+                if (item.Count() > 1) repeatedCharacters.Add(item.Key);
+            }
+            );
+
+            if (repeatedCharacters.Count < 2)
+            {
+                return "NO";
+            }
+
+            // initializing max  
+            // alphabet to 'a' 
+            char max = 'A';
+
+            // find largest alphabet 
+            for (int i = 0; i < repeatedCharacters.Count; i++)
+                if (repeatedCharacters[i] > max)
+                    max = repeatedCharacters[i];
+
+            // returning largest element 
+            return max.ToString();
+
+        }
+
+        public static void solution2(int N)
+        {
+            int enable_print = N % 10;
+            while (N > 0)
+            {
+                if (enable_print == 0 && N % 10 != 0)
+                {
+                    enable_print = 1;
+                //}
+                //else if (enable_print == 1)
+                //{
+                    Console.Write(N % 10);
+                }
+                N = N / 10;
+            }
         }
     }
 }
